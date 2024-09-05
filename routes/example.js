b80a1410eb9461c66dda59a3837bc349b0d9c2c1
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-05 17:22:01 +0700

    init project

--- routes/example.js
+++ routes/example.js
@@ -1,3 +1,21 @@
+/**
+ * @swagger
+ * /api/v1/hello_world:
+ *   get:
+ *     summary: Returns a resource
+ *     description: Get a simple resource from the API
+ *     responses:
+ *       200:
+ *         description: A resource object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: API Resource
+ */
 const express = require('express');
 const router = express.Router();
 
